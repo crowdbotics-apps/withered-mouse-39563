2343d6065c42d087d3a11cf3d86fb995785cd04e
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-04-28 20:57:43 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "withered-mouse-39563",
-  url: "https://withered-mouse-39563.botics.co",
-  api: "https://withered-mouse-39563.botics.co/api/v1"
-}
+  "name": "withered-mouse-39563",
+  "url": "https://withered-mouse-39563.botics.co",
+  "api": "https://withered-mouse-39563.botics.co/api/v1",
+  "initialRoute": "Untitled2"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
